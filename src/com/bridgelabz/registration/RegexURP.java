1513dc6567450e870230de99e19f1b6eb719a06d
Textspot <sachinkatarki1995@gmail.com>
2021-11-20 16:17:44 +0530

    Addes:User needs tO Enter a Valid First Name

--- src/com/bridgelabz/registration/RegexURP.java
+++ src/com/bridgelabz/registration/RegexURP.java
@@ -4,9 +4,14 @@ package com.bridgelabz.registration;
 public class RegexURP {
 
 	public static void main(String[] args) {
+		 System.out.println("Welcome to the User Registration Problem using Regex!!");
+	        UserOperator user = new UserOperator();
+	        user.firstName("Sachin");
+
+	    }
 	
 	}
 
- }
+ 
 
 
