f4a42262f4474088c26e195d07f0a948a2ffc32c
Textspot <sachinkatarki1995@gmail.com>
2021-11-20 16:28:02 +0530

    Addes:User needs to Enter the Last Name

--- src/com/bridgelabz/registration/RegexURP.java
+++ src/com/bridgelabz/registration/RegexURP.java
@@ -4,10 +4,10 @@ package com.bridgelabz.registration;
 public class RegexURP {
 
 	public static void main(String[] args) {
-		 System.out.println("Welcome to the User Registration Problem using Regex!!");
+		  System.out.println("Welcome to the User Registration Problem using Regex!!");
 	        UserOperator user = new UserOperator();
 	        user.firstName("Sachin");
-
+	        user.lastName("Santosh");
 	    }
 	
 	}
