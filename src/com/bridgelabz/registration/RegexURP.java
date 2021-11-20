dde37f64926305c93d5e6a126fbe232e45ea53cd
Textspot <sachinkatarki1995@gmail.com>
2021-11-20 16:37:10 +0530

    Addes:User needs tO Enter a Valid Email Name

--- src/com/bridgelabz/registration/RegexURP.java
+++ src/com/bridgelabz/registration/RegexURP.java
@@ -4,13 +4,15 @@ package com.bridgelabz.registration;
 public class RegexURP {
 
 	public static void main(String[] args) {
-		  System.out.println("Welcome to the User Registration Problem using Regex!!");
+		 System.out.println("Welcome to the User Registration Problem using Regex!!");
 	        UserOperator user = new UserOperator();
 	        user.firstName("Sachin");
 	        user.lastName("Santosh");
+	        user.emailaddress("Sachin@gmail.com");
 	    }
+  }
 	
-	}
+	
 
  
 
