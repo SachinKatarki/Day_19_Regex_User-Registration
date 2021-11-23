cd775fcc7d443cdeab2715de82713f08f50f7861
Sachin <90496023+SachinKatarki@users.noreply.github.com>
2021-11-24 00:53:01 +0530

    Update RegexURP.java

--- src/com/bridgelabz/registration/RegexURP.java
+++ src/com/bridgelabz/registration/RegexURP.java
@@ -7,7 +7,7 @@ public class RegexURP {
 		 System.out.println("Welcome to the User Registration Problem using Regex!!");
 	        UserOperator user = new UserOperator();
 	        user.firstName("Sachin");
-	        user.lastName("Santosh");
+	        user.lastName("Kumar");
 	        user.emailaddress("Sachin@gmail.com");
 	        user.phoneNo("91 9604315245");
 	        user.passward("Sachin@123");
